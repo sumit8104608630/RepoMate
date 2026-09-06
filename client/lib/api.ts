6137e54cd4a523eb11567d065a0ac89d45f1978f
Sumit sharma <sumit8104608630@gmail.com>
2026-09-06 19:02:48 +0530

    change the backend url to run globally

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -70,7 +70,7 @@ export class ApiError extends Error {
 }
 
 export function getApiBaseUrl() {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8081";
+  return process.env.NEXT_PUBLIC_API_BASE_URL || "https://repomate-bfie.onrender.com";
 }
 
 export function getGithubLoginUrl() {
