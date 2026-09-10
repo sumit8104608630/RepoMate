4d526abffdbe47edf940241fbbde6ed98bcea87c
Sumit sharma <sumit8104608630@gmail.com>
2026-09-10 17:53:03 +0530

    message error solve

--- backend/src/main/java/devPilot/backend/config/AppConfig.java
+++ backend/src/main/java/devPilot/backend/config/AppConfig.java
@@ -3,7 +3,7 @@ package devPilot.backend.config;
 import java.util.concurrent.Executor;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
